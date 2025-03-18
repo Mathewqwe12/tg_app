1d1f2ee98bd14b73886d9018d74a9e81391a6a28
Mathewqwe12 <esdefopa@gmail.com>
2025-03-18 15:09:57 +0300

    Обновлен URL для Vercel деплоя

--- backend/config.py
+++ backend/config.py
@@ -7,8 +7,8 @@ load_dotenv()
 # Токен бота
 BOT_TOKEN = os.getenv('BOT_TOKEN', '7364226532:AAH8YmUtoB-PmKLI1DcLUTRx2CRPakw7dzw')
 
-# URL веб-приложения - ОБНОВИТЕ ЭТОТ URL НА НОВЫЙ ИЗ NGROK (см. http://127.0.0.1:4040)
-WEBAPP_URL = os.getenv('WEBAPP_URL', 'https://6e92-84-17-55-151.ngrok-free.app/')
+# URL веб-приложения - Обновлено для Vercel
+WEBAPP_URL = os.getenv('WEBAPP_URL', 'https://tg-app-lqswsu2kd-mathews-projects-6af1e42a.vercel.app')
 
 # Настройки сервера
 HOST = os.getenv('HOST', '0.0.0.0')
